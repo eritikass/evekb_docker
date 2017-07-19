db60802fcd71ed8d545afa8a4d6fa0d3f08d6ddf
eritikass <eritikass@gmail.com>
2017-07-19 19:39:36 +0300

    log stuff for testing

--- check_online.sh
+++ check_online.sh
@@ -2,10 +2,18 @@
 
 c=$(curl http://localhost:8736)
 
+echo $(date +%Y-%m-%d:%H:%M:%S) " - run" >> cron.log
+
+if [[ $c != *"Unable to connect to mysql database."* ]]; then
+  echo $c >> cron.log
+fi
+
 if [[ $c != *"Connection refused"* ]] || [[ $c != *"Unable to connect to mysql database."* ]]; then
   # all ok
   exit 0
 fi
+
+echo $(date +%Y-%m-%d:%H:%M:%S) " - restart" >> cron.log
 
 echo $(date +%Y-%m-%d:%H:%M:%S) " - restart docker * start" >> restart.log
 
